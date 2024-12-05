f53548f7c1056e497b42a62bd8ab8f6105883a86
Valentina <vrudnitskaya@gmail.com>
2024-12-04 20:23:57 -0600

    [RB-47]add get and delete controllers and routes

--- src/controllers/savedBooksController.js
+++ src/controllers/savedBooksController.js
@@ -1,5 +1,5 @@
 const { StatusCodes } = require('http-status-codes');
-const { BadRequestError } = require('../errors');
+const { BadRequestError, NotFoundError } = require('../errors');
 const SavedBooks = require('../models/SavedBooks');
 
 const addBookToSavedBooks = async (req, res, next) => {
@@ -8,8 +8,10 @@ const addBookToSavedBooks = async (req, res, next) => {
     const { isbn10, isbn13 } = req.body;
 
     if (!isbn10 && !isbn13) {
-      throw new BadRequestError(
-        'At least one ISBN (ISBN-10 or ISBN-13) must be provided.'
+      return next(
+        new BadRequestError(
+          'At least one ISBN (ISBN-10 or ISBN-13) must be provided.'
+        )
       );
     }
 
@@ -51,6 +53,65 @@ const addBookToSavedBooks = async (req, res, next) => {
   }
 };
 
+const getSavedBooks = async (req, res, next) => {
+  try {
+    const { userId } = req.user;
+
+    const savedBooks = await SavedBooks.findOne({ user: userId });
+
+    if (!savedBooks) {
+      return next(new NotFoundError('No saved books found for this user.'));
+    }
+
+    res.status(StatusCodes.OK).json({
+      msg: 'Saved books fetched successfully.',
+      savedBooks,
+    });
+  } catch (error) {
+    console.error('Error fetching saved books:', error);
+    next(error);
+  }
+};
+
+const deleteBookFromSavedBooks = async (req, res, next) => {
+  try {
+    const { userId } = req.user; 
+    const { savedBookId } = req.body; 
+
+    if (!savedBookId) {
+      return next(new BadRequestError('Book ID is required to remove a book.'));
+    }
+
+    const updatedSavedBooks = await SavedBooks.findOneAndUpdate(
+      { user: userId }, 
+      { $pull: { books: { _id: savedBookId } } },
+      { new: true } 
+    );
+
+    if (!updatedSavedBooks) {
+      return next(new NotFoundError('No saved books found for this user.'));
+    }
+
+    const wasBookRemoved = !updatedSavedBooks.books.some(
+      (book) => book._id.toString() === savedBookId
+    );
+
+    if (!wasBookRemoved) {
+      return next(new NotFoundError('Book not found in saved list.'));
+    }
+
+    res.status(StatusCodes.OK).json({
+      msg: 'Book removed from saved list successfully.',
+      savedBooks: updatedSavedBooks,
+    });
+  } catch (error) {
+    console.error('Error removing book:', error);
+    next(error);
+  }
+};
+
 module.exports = {
   addBookToSavedBooks,
+  getSavedBooks,
+  deleteBookFromSavedBooks
 };
--- src/routes/savedBooksRouter.js
+++ src/routes/savedBooksRouter.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const auth = require('../middleware/authentication');
-const { addBookToSavedBooks,  } = require('../controllers/savedBooksController');
+const { addBookToSavedBooks, getSavedBooks, deleteBookFromSavedBooks } = require('../controllers/savedBooksController');
 
 router.post('/', auth, addBookToSavedBooks);
+router.get('/', auth, getSavedBooks);
+router.delete('/', auth, deleteBookFromSavedBooks);
 
 module.exports = router;
