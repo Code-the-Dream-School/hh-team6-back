4ea29164e9b0adf1e6cd9d59e008c4bef51ddccb
Valentina <vrudnitskaya@gmail.com>
2024-12-05 22:29:49 -0600

    [RB-47]add book details when user gets the list of saved books

--- src/controllers/savedBooksController.js
+++ src/controllers/savedBooksController.js
@@ -1,6 +1,7 @@
 const { StatusCodes } = require('http-status-codes');
 const { BadRequestError, NotFoundError } = require('../errors');
 const SavedBooks = require('../models/SavedBooks');
+const Book = require('../models/Book');
 
 const addBookToSavedBooks = async (req, res, next) => {
   try {
@@ -63,9 +64,56 @@ const getSavedBooks = async (req, res, next) => {
       return next(new NotFoundError('No saved books found for this user.'));
     }
 
+    const booksWithDetails = [];
+    
+    for (const book of savedBooks.books) {
+
+      const bookDetails = await Book.findOne({
+        $or: [{ isbn10: book.isbn10 }, { isbn13: book.isbn13 }],
+      });
+
+      if (bookDetails) {
+        const bookData = {
+          isbn10: bookDetails.isbn10,
+          isbn13: bookDetails.isbn13,
+          title: bookDetails.title,
+          author: bookDetails.author,
+          listings: [],
+        };
+
+        const booksWithSameIsbn = await SavedBooks.find({
+          "books.isbn10": book.isbn10,
+          "books.isbn13": book.isbn13,
+        });
+
+        booksWithSameIsbn.forEach(savedBook => {
+          savedBook.books.forEach(savedBookDetail => {
+            if (
+              savedBookDetail.isbn10 === book.isbn10 ||
+              savedBookDetail.isbn13 === book.isbn13
+            ) {
+              bookData.listings.push({
+                userId: bookDetails.createdBy,
+                bookIdOriginal: bookDetails._id,
+                bookIdInSaved: savedBookDetail._id,
+                price: bookDetails.price,
+                isAvailable: bookDetails.isAvailable,
+                condition: bookDetails.condition,
+                coverImageUrl: bookDetails.coverImageUrl,
+              });
+            }
+          });
+        });
+
+        booksWithDetails.push(bookData);
+      }
+    }
+
     res.status(StatusCodes.OK).json({
       msg: 'Saved books fetched successfully.',
-      savedBooks,
+      savedBooks: {
+        books: booksWithDetails,
+      },
     });
   } catch (error) {
     console.error('Error fetching saved books:', error);
